7ccf4be8f445f470854b9d78dd6d391930490d97
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-02 18:16:01 +0900

    表示されている画像を編集可能にする

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,6 +3,11 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
     document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
