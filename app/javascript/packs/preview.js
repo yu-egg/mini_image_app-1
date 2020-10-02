b2f7cfb17051ac74bed40851db8ae34d64b5ed9d
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-02 18:04:56 +0900

    画像URLをimg要素のsrc属性に設定

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -9,5 +9,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // 表示する画像を生成
     const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
   });
 });
